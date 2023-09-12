2c287981d22a36e03b182f11e475fa2818b41187
Alisson <alissonalves29@gmail.com>
2023-09-12 11:52:25 -0300

    started project and created LogIn, LogOut functions

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
+
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <p>This is a protected route!</p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
+
+export default SetupPage;
